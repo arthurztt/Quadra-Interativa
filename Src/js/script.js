7a5d69cff1bbc6a6b2455ee9fe3144f6ed456cbb
JRenato2007 <zerenato1712@gmail.com>
2024-04-28 21:56:16 -0300

    foi colocado o switch que mudara o tema de fundo, alinhamento dos icones e a organização do painel do usuario.

--- Src/js/script.js
+++ Src/js/script.js
@@ -1,10 +1,12 @@
 function abrirMenu() {
   let menu = document.getElementById("menu");
   let text = document.querySelectorAll(".text, .text-ok");
+  let copy = document.getElementById("copy");
   //Fazendo a verificação e deixando a barra lateral maior
   if (menu.style.width == "5%") {
     menu.style.transition = "all 0.5s ease";
     menu.style.width = "15%";
+    copy.style.fontSize = "15px";
     //para cada classe text dentro da function text ela adiciona as classes
     text.forEach(function (text) {
       text.style.transition = "all 0.5s ease";
@@ -14,6 +16,7 @@ function abrirMenu() {
   } else {
     menu.style.transition = "all 0.5s ease";
     menu.style.width = "5%";
+    copy.style.fontSize = "9px";
     //para cada classe text dentro da function text ela remove as classes
     text.forEach(function (text) {
       text.style.transition = "all 0.5s ease";
