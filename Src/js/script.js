7d0335df03d283e45bc2475f5ab1a43666ef1c38
arthurztt <artkikk1@gmail.com>
2024-05-02 14:35:28 -0300

    Finalização da barra lateral

--- Src/js/script.js
+++ Src/js/script.js
@@ -1,8 +1,8 @@
 function abrirMenu() {
-  let menu = document.getElementById("menu");
+  let menu = document.querySelector("section.menu");
   let text = document.querySelectorAll(".text, .text-ok");
   //Fazendo a verificação e deixando a barra lateral maior
-  if (menu.style.width == "5%") {
+  if (menu.style.width == "7%") {
     menu.style.transition = "all 0.4s ease"; // Alterei o tempo de transição para que a barra maior acompanhe as caixas to texto
     menu.style.width = "15%";
     //para cada classe text dentro da function text ela adiciona as classes
@@ -13,10 +13,10 @@ function abrirMenu() {
     });
   } else {
     menu.style.transition = "all 0.5s ease";
-    menu.style.width = "5%";
+    menu.style.width = "7%";
     //para cada classe text dentro da function text ela remove as classes
     text.forEach(function (text) {
-      text.style.transition = "all 0.4s ease";
+      text.style.transition = "all 0.3s ease";
       text.classList.add("text");
       text.classList.remove("text-ok");
     });
