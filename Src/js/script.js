35ac6f04003ea6261b6f1c3dda38587e959c2c36
arthurztt <artkikk1@gmail.com>
2024-05-01 23:31:56 -0300

    Alterações gerais na estilização da barra lateral, remoção da parte de copyright, e pequenas mudanças no js

--- Src/js/script.js
+++ Src/js/script.js
@@ -1,12 +1,10 @@
 function abrirMenu() {
   let menu = document.getElementById("menu");
   let text = document.querySelectorAll(".text, .text-ok");
-  let copy = document.getElementById("copy");
   //Fazendo a verificação e deixando a barra lateral maior
   if (menu.style.width == "5%") {
-    menu.style.transition = "all 0.5s ease";
+    menu.style.transition = "all 0.4s ease"; // Alterei o tempo de transição para que a barra maior acompanhe as caixas to texto
     menu.style.width = "15%";
-    copy.style.fontSize = "15px";
     //para cada classe text dentro da function text ela adiciona as classes
     text.forEach(function (text) {
       text.style.transition = "all 0.5s ease";
@@ -16,10 +14,9 @@ function abrirMenu() {
   } else {
     menu.style.transition = "all 0.5s ease";
     menu.style.width = "5%";
-    copy.style.fontSize = "9px";
     //para cada classe text dentro da function text ela remove as classes
     text.forEach(function (text) {
-      text.style.transition = "all 0.5s ease";
+      text.style.transition = "all 0.4s ease";
       text.classList.add("text");
       text.classList.remove("text-ok");
     });
