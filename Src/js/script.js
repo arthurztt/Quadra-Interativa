ab76ba05dce359b0ae9eb08e8dd3992089b678fa
arthurztt <artkikk1@gmail.com>
2024-05-15 15:35:38 -0300

    Fim da estilização dos dropdowns, e adição da animação quando aberto

--- Src/js/script.js
+++ Src/js/script.js
@@ -2,8 +2,8 @@ function abrirMenu() {
   const menu = document.querySelector("aside.menu");
   const text = document.querySelectorAll(".text, .text-ok");
   const lateralButton = document.getElementById("lateralButton");
-  const iconButton = document.querySelector(".button-img")
-  //Tag main 
+  const iconButton = document.querySelector(".button-img");
+  //Tag main
   const main = document.querySelector("main");
   //Fazendo a verificação e deixando a barra lateral maior
   if (menu.style.width == "132px") {
@@ -11,12 +11,12 @@ function abrirMenu() {
     menu.style.transition = "all 0.4s ease";
     menu.style.width = "282px";
     //Botão para a direita
-    iconButton.src = ("Src/images/arrow-left-circle-fill.svg")
+    iconButton.src = "Src/images/arrow-left-circle-fill.svg";
 
-    //Tamanho do main 
+    //Tamanho do main
     main.style.transition = "all  0.5s ease ";
     main.style.width = "100%";
-    //Alteração da posição do botão 
+    //Alteração da posição do botão
     lateralButton.style.left = "250px";
     lateralButton.style.transition = "all 0.3s ease";
     //para cada classe text dentro da function text ela adiciona as classes
@@ -26,12 +26,12 @@ function abrirMenu() {
       text.classList.add("text-ok");
     });
   } else {
-    //Alterações da barra lateral 
+    //Alterações da barra lateral
     menu.style.transition = "all 0.4s ease";
     menu.style.width = "132px";
-    //Botão para a esquerda 
-    iconButton.src = ("Src/images/arrow-right-circle-fill.svg")
-    lateralButton.style.left = "0px"
+    //Botão para a esquerda
+    iconButton.src = "Src/images/arrow-right-circle-fill.svg";
+    lateralButton.style.left = "0px";
     //Alterações mains
     main.style.width = "100%";
     //para cada classe text dentro da function text ela remove as classes
@@ -42,36 +42,67 @@ function abrirMenu() {
     });
   }
 }
-//Função dos eventos. Adiciona uma classe para a lista ser visível e ou não, com verificador. 
+//Função dos eventos. Adiciona uma classe para a lista ser visível e ou não, com verificador.
 function openEve() {
+  const ddHeader = document.querySelector("div#dd-events");
   const select = document.querySelector("div#events");
+  const arrow = document.querySelector("img#arrow-eve");
   if (select.classList.contains("list-close")) {
     select.classList.remove("list-close");
     select.classList.add("list-open");
+    ddHeader.style.borderBottomLeftRadius = "0px";
+    ddHeader.style.borderBottomRightRadius = "0px";
+    arrow.style.transform = "rotate(180deg)";
+    arrow.style.transition = "0.1s";
   } else {
     select.classList.add("list-close");
     select.classList.remove("list-open");
+    ddHeader.style.borderBottomLeftRadius = "15px";
+    ddHeader.style.borderBottomRightRadius = "15px";
+    arrow.style.transform = "rotate(0deg)";
+    arrow.style.transition = "0.1s";
   }
 }
-//Função dos esportes. Adiciona uma classe para a lista ser visível e ou não, com verificador.  
+//Função dos esportes. Adiciona uma classe para a lista ser visível e ou não, com verificador.
 function openEsp() {
+  const ddHeader = document.querySelector("div#dd-sport");
   const select = document.querySelector("div#esport");
+  const arrow = document.querySelector("img#arrow-esp");
   if (select.classList.contains("list-close")) {
     select.classList.remove("list-close");
     select.classList.add("list-open");
+    ddHeader.style.borderBottomLeftRadius = "0px";
+    ddHeader.style.borderBottomRightRadius = "0px";
+    arrow.style.transform = "rotate(180deg)";
+    arrow.style.transition = "0.1s";
   } else {
     select.classList.add("list-close");
     select.classList.remove("list-open");
+    ddHeader.style.borderBottomLeftRadius = "15px";
+    ddHeader.style.borderBottomRightRadius = "15px";
+    arrow.style.transform = "rotate(0deg)";
+    arrow.style.transition = "0.1s";
   }
 }
-//Função do Reservar a quadra. Adiciona uma classe para a lista ser visível e ou não, com verificador.  
+//Função do Reservar a quadra. Adiciona uma classe para a lista ser visível e ou não, com verificador.
 function openRes() {
+  const ddHeader = document.querySelector("div#dd-reserve");
   const select = document.querySelector("div#reserv ");
+  const arrow = document.querySelector("img#arrow-res");
+
   if (select.classList.contains("list-close")) {
     select.classList.remove("list-close");
     select.classList.add("list-open");
+    ddHeader.style.borderBottomLeftRadius = "0px";
+    ddHeader.style.borderBottomRightRadius = "0px";
+    arrow.style.transform = "rotate(180deg)";
+    arrow.style.transition = "0.1s";
   } else {
     select.classList.add("list-close");
     select.classList.remove("list-open");
+    ddHeader.style.borderBottomLeftRadius = "20px";
+    ddHeader.style.borderBottomRightRadius = "20px";
+    arrow.style.transform = "rotate(0deg)";
+    arrow.style.transition = "0.1s";
   }
 }
